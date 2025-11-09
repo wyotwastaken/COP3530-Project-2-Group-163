f848df8a468a946cd4a5e10dc037dd5626484289
wyotwastaken <wyatt.scheinbaum@ufl.edu>
2025-11-08 20:16:19 -0500

    Reordered main

--- resources/src/main.cpp
+++ resources/src/main.cpp
@@ -13,7 +13,7 @@ int main() {
     string word_txt = "../data/word_list.txt";
 
     Words words;
-    cout << "Loading words" << flush; // Read about std::flush here: https://en.cppreference.com/w/cpp/io/manip/flush.html
+    cout << "Loading words..." << endl;
 
     // chrono usage referenced from https://stackoverflow.com/questions/22387586/measuring-execution-time-of-a-function-in-c.
     auto t1 = chrono::high_resolution_clock::now();
@@ -23,12 +23,11 @@ int main() {
     auto ms_int = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
     auto s_int = chrono::duration_cast<chrono::seconds>(t2 - t1).count();
 
-    cout << endl;
     cout << "Loaded " << words.getWords().size() << " words!" << endl;
     cout << "Execution time: " << ms_int << " milliseconds. (" << s_int << " seconds)" << endl;
 
     // Construct Ball Tree
-    cout << "Constructing ball tree" << flush;
+    cout << "Constructing ball tree..." << endl;
     BallTree ball_tree;
 
     auto t3 = chrono::high_resolution_clock::now();
@@ -38,19 +37,29 @@ int main() {
     auto ms_int_2 = chrono::duration_cast<chrono::milliseconds>(t4 - t3).count();
     auto s_int_2 = chrono::duration_cast<chrono::seconds>(t4 - t3).count();
 
-    cout << endl;
     cout << "Ball tree constructed!" << endl;
     cout << "Execution time: " << ms_int_2 << " milliseconds. (" << s_int_2 << " seconds)" << endl;
 
+    cout << "Constructing KD tree..." << endl;
+    KDTree kd(words.getWords(), 128);
+
+    auto t7 = chrono::high_resolution_clock::now();
+    kd.build();
+    auto t8 = chrono::high_resolution_clock::now();
+
+    auto ms_int_4 = chrono::duration_cast<chrono::milliseconds>(t8 - t7).count();
+    auto s_int_4 = chrono::duration_cast<chrono::seconds>(t8 - t7).count();
+
+    cout << "KD tree constructed!" << endl;
+    cout << "Execution time: " << ms_int_4 << " milliseconds. (" << s_int_4 << " seconds)" << endl;
+
     // Semantic knn search input
-    bool running = true;
     string w = "";
     string neighbors = "";
-    while (running) {
-        cout << "Enter new word to generate semantic neighbor list (type '0' to exit): ";
+    while (true) {
+        cout << "Enter new word to generate semantic neighbor list (type '0' to exit into K-D Tree): ";
         cin >> w;
         if (w == "0") {
-            running = false;
             break;
         }
 
@@ -70,21 +79,15 @@ int main() {
         auto t6 = chrono::high_resolution_clock::now();
 
         auto ms_int_3 = chrono::duration_cast<chrono::milliseconds>(t6 - t5).count();
-        auto s_float_3 = chrono::duration_cast<chrono::seconds>(t6 - t5).count();
 
         cout << endl;
         cout << "Execution time: " << ms_int_3 << " milliseconds" << endl;
         cout << endl;
     }
 
-    cout << "Constructing KD tree" << flush;
-    KDTree kd(words.getWords(), 128);
-    kd.build();
-    cout << endl << "KD tree constructed!" << endl;
-
     //input output
     while(true) {
-        cout << "Enter new word to generate semantic neighbor list (type '0' to exit): ";
+        cout << "Enter new word to generate semantic neighbor list (type '0' to exit program): ";
         string w; if(!(cin >> w) || w == "0") break;
 
         cout << "Enter number of neighbors to search: ";
@@ -104,14 +107,21 @@ int main() {
             cout << "Please enter a valid word...\n"; continue;
         }
 
+        auto t9 = chrono::high_resolution_clock::now();
         auto res = kd.knn(D[qi].vec, (size_t)k);
+        auto t10 = chrono::high_resolution_clock::now();
+
+        auto ms_int_5 = chrono::duration_cast<chrono::milliseconds>(t10 - t9).count();
+
         sort(res.begin(), res.end(), [](auto& a, auto& b){ return a.second > b.second; });
         cout << "Searching for " << w << "'s nearest semantic neighbors..." << endl;
         cout << "Top " << res.size() << " semantically closest words to " << w << " (K-D Tree implementation):\n";
         for(size_t i = 0; i < res.size(); ++i) {
             cout << "[" << (i+1) << "] " << D[res[i].first].word << "\n";
         }
-        cout << "\n";
+        cout << endl;
+        cout << "Execution time: " << ms_int_5 << " milliseconds" << endl;
+        cout << endl;
     }
     return 0;
 }
